780cd046ef2438cca6bde05e444342096f04ac9e
Alex Kharchenko <snowpardx@gmail.com>
2021-05-02 18:05:20 +0300

    Added OrderedProducerConsumerSink`T.cs

---
+++ CommonTypes/OrderedProducerConsumerSink`T.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CommonTypes
+{
+    public class OrderedProducerConsumerSink<T> where T: class
+    {
+        private Dictionary<int, T> results = new Dictionary<int, T>();
+        private object mutex = new object();
+        private bool isDead = false;
+        private int consumerBlockIndex = 0;
+
+        public void Enqueue(int index, T result)
+        {
+            if (result == null)
+            {
+                throw new ArgumentNullException(nameof(result));
+            }
+            lock (mutex)
+            {
+                if (isDead)
+                {
+                    throw new InvalidOperationException("Sink already stopped");
+                }
+                results.Add(index, result);
+                Monitor.Pulse(mutex);
+            }
+        }
+
+        public T Dequeue()
+        {
+            lock (mutex)
+            {
+                T result = null;
+                while (!isDead && !results.TryGetValue(consumerBlockIndex, out result))
+                {
+                    Monitor.Wait(mutex);
+                }
+                if (result != null)
+                {
+                    results.Remove(consumerBlockIndex);
+                    consumerBlockIndex++;
+                    return result;
+                }
+
+
+                if (results.Count == 0)
+                {
+                    return null;
+                }
+
+                throw new InvalidOperationException("There are orphaned results in sink");
+            }
+        }
+
+        public void Stop()
+        {
+            lock (mutex)
+            {
+                isDead = true;
+                Monitor.PulseAll(mutex);
+            }
+        }
+    }
+}
