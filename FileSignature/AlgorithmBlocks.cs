d0b59104869f18a8642fb0526b22d26774b9efe2
Alex Kharchenko <snowpardx@gmail.com>
2021-05-03 01:47:38 +0300

    Added signature generator

---
+++ FileSignature/AlgorithmBlocks.cs
@@ -0,0 +1,90 @@
+﻿using CommonTypes;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace FileSignature
+{
+    class AlgorithmBlocks: IDisposable
+    {
+        private FileStream fileStream;
+        private BufferedStream bufferedStream;
+        private BinaryReader binaryReader;
+        private int blockLength;
+        private bool readingFinished = false;
+        private int blockNumber = 0;
+        private ThreadLocal<SHA256> hashAlgorithm = new ThreadLocal<SHA256>(trackAllValues: true);
+
+        private AlgorithmBlocks() { }
+
+        public static AlgorithmBlocks Init(string filePath, int blockLength)
+        {
+            var blocks = new AlgorithmBlocks();
+            try
+            {
+                blocks.blockLength = blockLength;
+                blocks.fileStream = File.OpenRead(filePath);
+                blocks.bufferedStream = new BufferedStream(blocks.fileStream);
+                blocks.binaryReader = new BinaryReader(blocks.bufferedStream);
+                return blocks;
+            } catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"Unable to find on the path: {filePath}");
+                throw new HandledException(e);
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach(var instance in hashAlgorithm.Values)
+            {
+                instance.Dispose();
+            }
+            hashAlgorithm.Dispose();
+            binaryReader?.Dispose();
+            bufferedStream?.Dispose();
+            fileStream?.Dispose();
+        }
+
+        public byte[] GetBlock()
+        {
+            if (readingFinished)
+            {
+                return null;
+            }
+            var block = binaryReader.ReadBytes(blockLength);
+            if(block.Length < blockLength)
+            {
+                readingFinished = true;
+                if(block.Length == 0)
+                {
+                    return null;
+                }
+            }
+            return block;
+        }
+
+        public string ProcessBlock(byte[] data)
+        {
+            if(!hashAlgorithm.IsValueCreated)
+            {
+                hashAlgorithm.Value = SHA256.Create();
+            }
+            var hash = hashAlgorithm.Value.ComputeHash(data);
+            // hash to string is from https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.hashalgorithm.computehash?view=net-5.0
+            var sBuilder = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sBuilder.Append(hash[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+        public void UseResult(string result)
+        {
+            Console.WriteLine($"{blockNumber}: {result}");
+            blockNumber++;
+        }
+    }
+}
--- FileSignature/Program.cs
+++ FileSignature/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using CommonTypes;
+using System;
 
 namespace FileSignature
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Invalid Syntax, use the following one 'signature <inputFile> <blockSizeInBytes>'");
+                return 1;
+            }
+            if (!int.TryParse(args[1], out int blockSizeInBytes))
+            {
+                Console.WriteLine($"Unable to parse block size from: '{args[1]}'");
+                return 1;
+            }
+            try
+            {
+                using(var blocks = AlgorithmBlocks.Init(args[0], blockSizeInBytes))
+                {
+                    var algorithm = new ParallelAlgorithm<byte[], string>(blocks.GetBlock, blocks.ProcessBlock, blocks.UseResult);
+                    return algorithm.Run();
+                }
+
+            } catch (HandledException e)
+            {
+                return 1;
+            } catch (Exception e)
+            {
+                Console.WriteLine($"Unhandled exception: {e.Message}");
+                return 1;
+            }
         }
     }
 }
