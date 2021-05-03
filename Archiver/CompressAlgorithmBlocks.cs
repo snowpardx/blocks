c4729a8949224e3e0a0aa96291663e5f1eb6fbad
Alex Kharchenko <snowpardx@gmail.com>
2021-05-03 14:16:23 +0300

    Added archiver compress function

---
+++ Archiver/CompressAlgorithmBlocks.cs
@@ -0,0 +1,95 @@
+﻿using CommonTypes;
+using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Archiver
+{
+    class CompressAlgorithmBlocks: IDisposable
+    {
+        private FileStream inputFileStream;
+        private BufferedStream inputBufferedStream;
+        private BinaryReader binaryReader;
+        private FileStream outputFileStream;
+        private BufferedStream outputBufferedStream;
+        private BinaryWriter binaryWriter;
+        private const int blockLength = 1024 * 1024; // 1MiB
+        private bool readingFinished = false;
+        private const double resizeFactor = 1.25; // used to set initial memory stream size in case zipped data would take more memory
+
+        private CompressAlgorithmBlocks() { }
+
+        public static CompressAlgorithmBlocks Init(string inputFilePath, string outputFilePath)
+        {
+            var blocks = new CompressAlgorithmBlocks();
+            try
+            {
+                blocks.inputFileStream = File.OpenRead(inputFilePath);
+                blocks.inputBufferedStream = new BufferedStream(blocks.inputFileStream);
+                blocks.binaryReader = new BinaryReader(blocks.inputBufferedStream);
+            } catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"Unable to find on the path: {inputFilePath}");
+                throw new HandledException(e);
+            }
+            try
+            {
+                blocks.outputFileStream = File.Open(outputFilePath, FileMode.Create);
+                blocks.outputBufferedStream = new BufferedStream(blocks.outputFileStream);
+                blocks.binaryWriter = new BinaryWriter(blocks.outputBufferedStream);
+            } catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"Unable to create file on the path: {outputFilePath}");
+                throw new HandledException(e);
+            }
+            return blocks;
+        }
+
+        public void Dispose()
+        {
+            binaryReader?.Dispose();
+            inputBufferedStream?.Dispose();
+            inputFileStream?.Dispose();
+
+            binaryWriter?.Dispose();
+            outputBufferedStream?.Dispose();
+            outputFileStream?.Dispose();
+        }
+
+        public byte[] GetBlock()
+        {
+            if (readingFinished)
+            {
+                return null;
+            }
+            var block = binaryReader.ReadBytes(blockLength);
+            if(block.Length < blockLength)
+            {
+                readingFinished = true;
+                if(block.Length == 0)
+                {
+                    return null;
+                }
+            }
+            return block;
+        }
+
+        public byte[] ProcessBlock(byte[] data)
+        {
+            using (MemoryStream resultStream = new MemoryStream((int)(data.Length * resizeFactor)))
+            {
+                using (GZipStream zipStream = new GZipStream(resultStream, CompressionMode.Compress))
+                {
+                    zipStream.Write(data);
+                    return resultStream.ToArray();
+                }
+            }
+        }
+        public void UseResult(byte[] result)
+        {
+            binaryWriter.Write(result.Length); // we would use this when decompress, so that read the data corresponding to compressed block
+            binaryWriter.Write(result);
+        }
+
+    }
+}
---
+++ Archiver/Program.cs
@@ -0,0 +1,46 @@
+﻿using CommonTypes;
+using System;
+
+namespace Archiver
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Invalid Syntax, use the following one 'archiver compress|decompress <inputFile> <outputFile>'");
+                return 1;
+            }
+            bool compress = false;
+            if (string.Compare(args[0], "compress", true) == 0)
+            {
+                compress = true;
+            } else if (string.Compare(args[0], "decompress", true) == 0)
+            {
+                compress = false;
+            } else
+            {
+                Console.WriteLine($"Expected 'compress' or 'decompress' command, but found '{args[0]}'");
+                return 1;
+            }
+            try
+            {
+                using(var blocks = CompressAlgorithmBlocks.Init(args[1], args[2]))
+                {
+                    var algorithm = new ParallelAlgorithm<byte[], byte[]>(blocks.GetBlock, blocks.ProcessBlock, blocks.UseResult);
+                    return algorithm.Run();
+                }
+
+            } catch (HandledException e)
+            {
+                return 1;
+            } catch (Exception e)
+            {
+                Console.WriteLine($"Unhandled exception: {e.Message}");
+                return 1;
+            }
+
+        }
+    }
+}
